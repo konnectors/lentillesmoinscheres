3dc523c21ad3d9ddd3a9179f449c5a8c1ebb779c
Cyrille Perois <cperois@outlook.com>
2018-12-07 18:33:40 +0100

    feat: Fetch bills

--- src/index.js
+++ src/index.js
@@ -7,57 +7,34 @@ const {
   log
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very useful for
-  // debugging but very verbose. That is why it is commented out by default
-  // debug: true,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
   cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
   json: false,
-  // this allows request-promise to keep cookies between requests
   jar: true
 })
+const omit = require('lodash/omit')
 
 const baseUrl = 'https://www.lentillesmoinscheres.com'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.email, fields.password)
   log('info', 'Successfully logged in')
-  // // The BaseKonnector instance expects a Promise as return of the function
-  // log('info', 'Fetching the list of documents')
-  // const $ = await request(`${baseUrl}/index.html`)
-  // // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  // log('info', 'Parsing list of documents')
-  // const documents = await parseDocuments($)
 
-  // // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // // common case in connectors
-  // log('info', 'Saving data to Cozy')
-  // await saveBills(documents, fields, {
-  //   // this is a bank identifier which will be used to link bills to bank operations. These
-  //   // identifiers should be at least a word found in the title of a bank operation related to this
-  //   // bill. It is not case sensitive.
-  //   identifiers: ['books']
-  // })
+  const bills = await getBills()
+
+  log('info', 'Saving data to Cozy')
+  await saveBills(bills, fields, {
+    identifiers: ['lentilles']
+  })
 }
 
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
 function authenticate(email, password) {
   return signin({
     url: `${baseUrl}/client_login.php`,
     formSelector: '#loginModal-form',
     formData: { email, password },
-    // the validate function will check if the login request was a success. Every website has
-    // different ways respond: http status code, error message in html ($), http redirection
-    // (fullResponse.request.uri.href)...
     validate: (statusCode, $, fullResponse) => {
       if (fullResponse.request.uri.href === `${baseUrl}/client/home.php`) {
         return true
@@ -69,53 +46,58 @@ function authenticate(email, password) {
   })
 }
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
+async function getBills() {
+  const $ = await request(`${baseUrl}/client/orders.php?filterInterval=all`)
+
   const docs = scrape(
     $,
     {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
+      number: '.orderRow-orderID span',
       amount: {
-        sel: '.price_color',
-        parse: normalizePrice
+        sel: '.orderRow-data:nth-child(2)',
+        parse: parseAmount
       },
       fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
+        sel: '.display-invoice',
+        attr: 'href',
+        parse: href => `${baseUrl}/client/${href}`
       },
-      filename: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: title => `${title}.jpg`
+      date: {
+        sel: '.orderRow-data:nth-child(1)',
+        parse: parseDate
       }
     },
-    'article'
+    '.orderRow'
   )
+
+  const attrsNotToSave = ['number']
+
   return docs.map(doc => ({
-    ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
+    ...omit(doc, attrsNotToSave),
+    filename: getFileName(doc),
+    type: 'health_costs',
+    vendor: 'Lentilles Moins Cheres',
     currency: '€',
-    vendor: 'template',
     metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // useful for debugging or data migration
       importDate: new Date(),
-      // document version, useful for migration after change of document structure
       version: 1
     }
   }))
 }
 
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
+function parseAmount(rawAmount) {
+  return parseFloat(rawAmount.replace(/[a-zA-Z: €]*/g, '').replace(',', '.'))
+}
+
+function parseDate(rawDate) {
+  const [day, month, year] = rawDate.replace(/[a-zA-Z: ]/g, '').split('/')
+  const date = new Date(2000 + parseInt(year), month - 1, day)
+
+  return date
+}
+
+function getFileName(doc) {
+  return `${doc.date.toISOString().slice(0, 10)}_${doc.number}_${
+    doc.amount
+  }.pdf`
 }
